692997cef1e3f3a8576c069ac60783b83f4873c3
N1hron <denik8008@gmail.com>
2023-04-25 17:00:38 +0300

    added skeleton

---
+++ src/components/skeleton/Skeleton.jsx
@@ -0,0 +1,21 @@
+import './skeleton.scss';
+
+export default function Skeleton() {
+    return (
+        <div className="skeleton">
+            <div className="skeleton__item">
+                <div className="skeleton__header"></div>
+                <div className="skeleton__info"></div>
+                <div className="skeleton__img"></div>
+                <div className="skeleton__footer"></div>
+            </div>
+            <div className="skeleton__item skeleton__item_wide">
+            <div className="skeleton__header"></div>
+                <div className="skeleton__info"></div>
+                <div className="skeleton__info"></div>
+                <div className="skeleton__footer"></div>
+            </div>
+            {/* <div className="skeleton__item"></div> */}
+        </div>
+    )
+}
--- src/utils/renderData.jsx
+++ src/utils/renderData.jsx
@@ -1,8 +1,12 @@
 import Loading from '../components/loading/Loading';
 import Error from '../components/error/Error';
+import Skeleton from '../components/skeleton/Skeleton';
 
 export default function renderData(Component, process) {
     switch(process) {
+        case 'idle': {
+            return <Skeleton/>
+        }
         case 'error': {
             return <Error/>;
         }
